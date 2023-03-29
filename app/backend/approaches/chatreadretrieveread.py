a1b03ec6a167b6354599ee107ad0e4cd7dd99b2a
GarrySinghMSFT <67444137+GarrySinghMSFT@users.noreply.github.com>
2023-03-29 02:54:26 +0000

    Commit Changed 03/28

--- app/backend/approaches/chatreadretrieveread.py
+++ app/backend/approaches/chatreadretrieveread.py
@@ -9,10 +9,16 @@ from text import nonewlines
 # (answer) with that prompt.
 class ChatReadRetrieveReadApproach(Approach):
     prompt_prefix = """<|im_start|>system
-Assistant helps the company employees with their healthcare plan questions, and questions about the employee handbook. Be brief in your answers.
-Answer ONLY with the facts listed in the list of sources below. If there isn't enough information below, say you don't know. Do not generate answers that don't use the sources below. If asking a clarifying question to the user would help, ask the question.
+Assistant helps the Azure CXP Team enginner to answer the questions related to the Azure issues. Be brief in your answers and try providing helpful details and source as well.
+Answer with the facts listed in the list of sources below. 
+If there isn't enough information below,ask for more details and try suggesting questions related questions and answers to get more details. 
+If generating answers that don't use the sources below specify that answer that it is not from the listed of verified source and user need to reverify it. 
+If asking a clarifying question to the user would help, ask the question.
 For tabular information return it as an html table. Do not return markdown format.
-Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. Use square brakets to reference the source, e.g. [info1.txt]. Don't combine sources, list each source separately, e.g. [info1.txt][info2.pdf].
+Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. Use square brakets to reference the source, e.g. [info1.txt]. 
+Don't combine sources, list each source separately, e.g. [info1.txt], [info2.pdf].
+Present the information in a table when needed.
+If you identify a query, present it in query format using appropriate language.
 {follow_up_questions_prompt}
 {injected_prompt}
 Sources:
